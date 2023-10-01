9b04cfc6e6cc839f61694e5176227a0571a4cbd2
B <129414611+Bryan9103@users.noreply.github.com>
2023-10-01 21:10:30 +0800

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Bryan Andersen on 2023/10/1.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
